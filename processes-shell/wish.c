e17a2c5d01b060ddd826559a6ca877458d0bf636
Joel Foster <foster@royal-13.cs.wisc.edu>
2021-10-10 19:18:57 -0500

    cleaned up built in commands, ready for cd and path imp

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -3,25 +3,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 int main(int argc, char *argv[]){
 
     /// GLOBAL VARS
     // TODO REIMPLEMENT WITH AN ARRAY OF PATHS
     char *path = "/bin";
 
-    static char* built_in_cmds[3] = {"cd","exit","path"};
+    static char* built_in_cmds[3] = {"exit","cd","path"};
     int num_bic = 3;
     
     //check cl args
     if(argc > 2){
         printf("error: too many arguments provided\n");
-        return 0;//error, more than a single argument
+        exit(1);//error, more than a single argument
     }
     if(argc == 2){
         //batch mode, second argument must be a text file to read input from
         if(strcmp(argv[0],"./wish")== 0){
             //printf("Two arguments: Batch Mode with argument: %s \n",argv[1]);
         }
+        // try to open file and store txt -> if fails, exit(1)
     }else{
         //interactive mode
         //while loop - condition: if the next input is not exit        FILE *stream;
@@ -53,28 +55,48 @@ int main(int argc, char *argv[]){
                 ;   
             // for (int c = 0; c < i; c++)
             //     printf(" arg %d : [%s]\n", c, dest[c]); 
+                        int num_args = i;
+            printf("Num args: %i\n", num_args);
 
-            //parsed args in dest
+            //parsed args in dest, cleaned so no newline
 
             // BUILT-IN-COMMANDS CHECK
             for(int i = 0; i < num_bic; i++){
                 if(strcmp(dest[0], built_in_cmds[i]) == 0){
                     //process command
-                    char *proc = built_in_cmds[i];
                     //continue since we know command is built in
                     // (don't need to check for other commands)
-                    if(strcmp(proc, "exit")){
+                    if(i == 0){
                         //call exit
+                        exit(0);
                     }
-                    if(strcmp(proc, "cd")){
+                    if(i == 1){
+                        /*cd: cd always take one argument (0 or >1 args should be 
+                        signaled as an error). To change directories, 
+                        use the chdir() system call with the argument 
+                        supplied by the user; if chdir fails, 
+                        that is also an error.*/
                         //call cd
                     }
-                    if(strcmp(proc, "path")){
-                        //call path
+                    if(i == 2){
+                        /*path: The path command takes 0 or more arguments, 
+                        with each argument separated by whitespace 
+                        from the others. A typical usage would be like this: 
+                        wish> path /bin /usr/bin, 
+                        which would add /bin and /usr/bin to the search path of the shell. 
+                        If the user sets path to be empty, 
+                        then the shell should not be able to run any programs 
+                        (except built-in commands). 
+                        The path command always overwrites the old path
+                         with the newly specified path.*/
                     }
-                    continue;
+                    break;
                 }
             }
+
+            // NOT A BUILT IN COMMAND -> check for corresponding binary in path
+
+            // TODO REIMPLEMENT -> GENERALIZE AND SEARCH PATH FOR ARG
             // check if cmd: LS
             if(strcmp(dest[0],"ls")==0){
                 //run ls proc
@@ -106,9 +128,13 @@ int main(int argc, char *argv[]){
                     write(STDERR_FILENO, error_message, strlen(error_message)); 
                 }
             }
+            // ARG NOT FOUND IN PATH
+            // -> RAISE ERROR?
+            // char error_message[30] = "An error has occurred\n";
+            // write(STDERR_FILENO, error_message, strlen(error_message));
+            //break; //exits loop and exits program 
+            //END OF WHILE
         }
-
-
 
         free(line);
         fclose(stdin);
